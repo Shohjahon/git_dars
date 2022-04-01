67a9077f145709b5877283fcec4e01c2e94515b6
Shohjahon Rahmataliev <r.shohjahon@gmail.com>
2022-04-01 09:21:08 +0500

    #2 commit

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ public class Main{
 
 	public static void main(String[] args){
 		//this prints message
+		//sdfjdskfjsdkljflsd
+		dsfjklsdfjlsdkfj
+		dfjsdlkfjsdlfkjsdlf
+		sdlfkjhsdkfljsdfklsdj
+		kjdsfhksdjfhsdk
 		System.out.println("Hello World!");
+		sdfjaskldfjldskjf
+		kjdlfkjsdlfjd
+		kjsdlfkjsdflkdk
 	}
 }
