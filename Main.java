f7792a03b7839c8fde06c22192e26c06a8733539
Shohjahon Rahmataliev <r.shohjahon@gmail.com>
2022-04-01 10:20:11 +0500

    #1

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main{
 
 	public static void main(String[] args){
 		//this is conditional statement
+		jfsdjflsdkjfglsdkg
+		jkgksdfghkfdhgkdfjhgfdkh
 		if (t == null) {
 			log.error(new Exception("Error"))
 		}
